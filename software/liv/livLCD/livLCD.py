b0545be3d6487294d04e479860fa51f58a2db384
alfredc333 <alfred.chioiu@firstcypress.com>
2015-09-10 13:52:20 +0800

    fix for livLCD crash without CO2 sensor onboard

--- software/liv/livLCD/livLCD.py
+++ software/liv/livLCD/livLCD.py
@@ -26,8 +26,6 @@ LCDPort = config.getint('LCD_DISPLAY', 'port_no')
 displayTime = config.getfloat('LCD_CYCLE_TIME', 'display_time')
 displayCycles = config.getint('LCD_CYCLE_TIME', 'no_of_cycles')
 
-displayStrings = ["Temperature", "Humidity", "CO2 Level", "Air Pressure"]
-
 
 logging.config.fileConfig('logging.ini')
 logger = logging.getLogger(__name__)
@@ -42,77 +40,103 @@ try:
   lcd.initDisplay()
   logger.info('LCD initialized')
 except:
-  logger.error('Failed to initialize LCD')
-  
-# read last readings directly from database, don't use local APIs
+  logger.critical('Failed to initialize LCD')
+  sys.exit(1)
+
+# read last readings directly from database, so I can have LCD working 
+# even when livAPIs is down so:
 # initialize db connection 
 # conect to sqlite DB
 try:
   dbconn = sqlite3.connect('../livDB/liv.db')
   dbconn.row_factory = sqlite3.Row
-
   logger.info('sqlite DB connection initialized')
 except:
-  logger.error('sqlite DB Failed to initialize')
+  logger.critical('sqlite DB Failed to initialize')
+  sys.exit(1)
+  
+#some variables here
+sensorNames = ['temperature', 'humidity', 'airpressure', 'co2']  
+displayStrings = {'temperature': "Temperature", 'humidity': "Humidity", 'airpressure': "Air Pressure", 'co2': "CO2 Level"}
+onBoardSensors = {'temperature' :False, 'humidity': False, 'airpressure': False, 'co2': False}
+config.read('../livDB/livDB.config')
+onBoardSensors['temperature'] = config.getboolean('ON_BOARD_SENSORS', 'temp_humidity_sensor')
+onBoardSensors['humidity'] = config.getboolean('ON_BOARD_SENSORS', 'temp_humidity_sensor')
+onBoardSensors['airpressure'] = config.getboolean('ON_BOARD_SENSORS', 'air_pressure_sensor')
+onBoardSensors['co2'] = config.getboolean('ON_BOARD_SENSORS', 'co2_sensor')
+logger.info('onBoardSensors:')
+logger.info(onBoardSensors)
   
 lcd.cleanFirstLine()
 lcd.cleanSecondLine()
     
 while(True):
   config.read('./livLCD.config')
-  #set alarm level
+  #read alarm levels from config file, currently blink measurement only if
+  #measured value is higher then threshold
   CO2_ALARM = config.getfloat('ALARM_LEVELS', 'co2')
-  TEMP_ALARM = config.getfloat('ALARM_LEVELS', 'temperature')
-  HUMID_ALARM = config.getfloat('ALARM_LEVELS', 'humidity')
+  T_ALARM = config.getfloat('ALARM_LEVELS', 'temperature')
+  H_ALARM = config.getfloat('ALARM_LEVELS', 'humidity')
   AP_ALARM = config.getfloat('ALARM_LEVELS', 'airPressure')
-  alarmLevels = [TEMP_ALARM, HUMID_ALARM, CO2_ALARM, AP_ALARM]
-    # read last readings directly from database, don't use local APIs  
-  dbData=[]
-  dbDisplayData=[]
-  alarmValues=[False, False, False, False]
+  alarmLevels = {'temperature':T_ALARM, 'humidity':H_ALARM, 'airpressure':AP_ALARM, 'co2':CO2_ALARM}
+  alarmValues={'temperature': False, 'humidity':False, 'airpressure':False, 'co2':False}
+  
+  
+  #read last measurement directly from database, don't use local APIs so LCD display works even if 
+  #LiV APIs process is down 
+  dbData={}
+  dbDisplayData={}
   
   try:
     logger.info("start retrieve last sqlite record")
     query = 'SELECT * from Measurements ORDER By Mid DESC LIMIT 1'
-    
     cursor = dbconn.execute(query)
     
     for row in cursor:
       #print row
-      temperature = str(row["Temperature"])
-      humidity = str(row["Humidity"])
-      airPressure = str(row["AirPressure"])
+      t = str(row["Temperature"])
+      h = str(row["Humidity"])
+      ap = str(row["AirPressure"])
       timeStamp = row["Timestamp"]
       co2 = str(row["CO2level"])
-      dbData =[temperature, humidity, co2, airPressure]
+      dbData ={'temperature':t, 'humidity':h, 'airpressure':ap, 'co2':co2}
+
       logger.info("Succesfully retrieved db Data: ")
-      logger.info("Temperature = " + temperature)
-      logger.info("Humidity = " + humidity)
-      logger.info("Air Pressure = " + airPressure)
-      logger.info("CO2 Level = " + co2)
       logger.info("Time Stamp = " + timeStamp)
       logger.info("alarmLevels are: ")
       logger.info(alarmLevels)
       logger.info("dbData is: ")
       logger.info(dbData)
       
-      for index, d in enumerate(dbData):
-        if (float(d) > alarmLevels[index]):
-           alarmValues[index] = True
-      dbDisplayData = [dbData[0]+' C', dbData[1]+' %',dbData[2]+' ppm',dbData[3]+' hPa']
+      for m, s in enumerate(sensorNames):
+        if (float(dbData[s]) > float(alarmLevels[s])):
+          alarmValues[s] = True
+      
+      #dbDisplayData = [dbData[0]+' C', dbData[1]+' %',dbData[2]+' ppm',dbData[3]+' hPa']
+      dbDisplayData = {'temperature': dbData['temperature']+' C','humidity': dbData['humidity']+' %', 'airpressure':dbData['airpressure']+ ' hPa', 'co2': dbData['co2']+' ppm'}
+      
+  
   except:
-    temperature ="error"
-    humidity ="error"
-    airPressure="error"
-    co2="error"
-    dbDisplayData =[temperature, humidity, co2, airPressure]
-    alarmValues = [True, True, True, True]
-    logger.error("Failed to retrieve record")
+    dbDisplayData ={'temperature':"error", 'humidity':"error", 'airpressure':"error", 'co2':"error"}
+    alarmValues = {'temperature':True, 'humidity':True, 'airpressure':True, 'co2':True}
+    logger.critical("Failed to retrieve record")
+    sys.exit(1)
   
-  lcdDisplayData = zip(displayStrings,dbDisplayData) 
-  
-  
+  #display only measurements for onBoard Sensors
 
+  dStrings = dict(displayStrings)
+  dData = dict(dbDisplayData)
+  aValues = dict(alarmValues)
+
+  for i, v in enumerate(sensorNames):
+    if onBoardSensors[v] == False:
+      del dStrings[v]
+      del dData[v]
+      del aValues[v]
+
+  lcdDisplayData = zip(dStrings.values(),dData.values()) 
+  
+  
   for x in range(0,displayCycles):
     i=0
     for idx, l in enumerate(lcdDisplayData):
@@ -122,7 +146,7 @@ while(True):
       lcd.cleanSecondLine()    
       
       lcd.writeFirstLine(l[0])
-      if (alarmValues[idx] == True):
+      if (aValues.values()[idx] == True):
         #blink every 0.5 seconds
         t=0
         while(t < displayTime):
